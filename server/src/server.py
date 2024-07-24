fa8dc9d58c33fbaf84a4508b950445d9342f26e0
MatheusR42 <matheus.araujo.r42@gmail.com>
2024-07-24 02:00:18 -0300

    fix python protocol

--- server/src/server.py
+++ server/src/server.py
@@ -1,11 +1,11 @@
 import grpc
 from concurrent import futures
 from protocols import meu_qoelho_mq_pb2_grpc
-from src import controller
+from controller import MeuQoelhoMqServicer
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    meu_qoelho_mq_pb2_grpc.add_MeuQoelhoMqServicer_to_server(controller.MeuQoelhoMqServicer(), server)
+    meu_qoelho_mq_pb2_grpc.add_MeuQoelhoMqServicer_to_server(MeuQoelhoMqServicer(), server)
     server.add_insecure_port("[::]:50051")
     server.start()
     print("running server")
