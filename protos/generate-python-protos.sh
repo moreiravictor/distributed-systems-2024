dc6db633f61e5844ed856ed8191af6ccc5129c1a
matheuaraujo_meli <matheus.araujo@mercadolivre.com>
2024-07-25 23:14:27 -0300

    fix shell script to work on mac and ubuntu

--- protos/generate-python-protos.sh
+++ protos/generate-python-protos.sh
@@ -2,20 +2,18 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 result_path="$parent_path/../server/src/protocols"
 
-# Detect the operating system
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS
-    sed_cmd="sed -i '' -e"
-else
-    # Assuming Linux (Ubuntu)
-    sed_cmd="sed -i -e"
-fi
-
 python3 -m grpc_tools.protoc -I. --python_out="$result_path" --pyi_out="$result_path" --grpc_python_out="$result_path" ./meu-qoelho-mq.proto
 
 find "$result_path" -name '*.py' | while read -r file_path; do
         file_name=$(basename "$file_path")
         package=${file_name%.*}
         echo "Found package ${package}. Beginning replacement"
-        find "$result_path" -name '*.py' -exec $sed_cmd "s/import ${package}/from protocols import ${package}/g" {} \;
+        # Detect the operating system
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+                # macOS
+                find "$result_path" -name '*.py' -exec sed -i '' -e "s/import ${package}/from protocols import ${package}/g" {} \;
+        else
+                # Assuming Linux (Ubuntu)
+                find "$result_path" -name '*.py' -exec sed -i -e "s/import ${package}/from protocols import ${package}/g" {} \;
+        fi
 done
